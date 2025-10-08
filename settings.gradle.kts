60e61cb288680e9b8b279c1e3c8f4f0ef528afba
Antonio <selvas.leon029@gmail.com>
2025-10-07 18:30:45 -0600

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "BariaPlus"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.AntonioSelvas
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
